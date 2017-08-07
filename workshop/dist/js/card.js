a30d1ca04b6b062cabcc3e685326eddf1a75c832
Mario Hernandez <mario.hernandez@mediacurrent.com>
2017-08-06 17:06:57 -0700

    Adds DCLA and Workshop Drupal themes

---
+++ workshop/dist/js/card.js
@@ -0,0 +1,15 @@
+!function (document, Drupal, $) {
+  'use strict';
+
+  /**
+   * Use this to describe what your behavior does.
+   */
+
+  Drupal.behaviors.card = {
+
+    attach: function attach(context) {
+      //...
+    }
+  };
+}(document, Drupal, jQuery);
+//# sourceMappingURL=card.js.map
